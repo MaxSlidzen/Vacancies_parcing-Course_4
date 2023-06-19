5d1a3a12e94b058b90f919715b61b402e2613275
Max Slidzen <slidzon94@gmail.com>
2023-06-19 04:12:22 +0300

    changed vacancies

--- src/classes/vacancies.py
+++ src/classes/vacancies.py
@@ -1,24 +1,19 @@
-
-import datetime
-
 class Vacancy:
     """
     Класс для работы с вакансиями
     """
-    def __init__(self, vacancy_name: str, salary: tuple, description: str, url: str, location: str,
-                 published: datetime, experience: str, requirements: str, education:str,
-                 employment: str) -> None:
+
+    def __init__(self, vacancy_name: str, salary: str, description: str, url: str, location: str,
+                 published: str, experience: str, employment: str) -> None:
         """
         Инициализация объекта Vacancy.
         :param vacancy_name: Название вакансии
-        :param salary: Сумма зарплаты (от, до)
+        :param salary: Сумма зарплаты
         :param description: Описание вакансии
         :param url: Ссылка на вакансию
         :param location: Город
         :param published: Дата публикации вакансии
         :param experience: Требуемый опыт работы
-        :param requirements: Требования к вакансии
-        :param education: Требуемое образование
         :param employment: Тип занятости и график
         """
         self.vacancy_name = vacancy_name
@@ -28,20 +23,16 @@ class Vacancy:
         self.location = location
         self.published = published
         self.experience = experience
-        self.requirements = requirements
-        self.education = education
         self.employment = employment
-
 
     def __repr__(self) -> str:
         """
         Возвращает строковое представление класса для разработчика
         :return: строковое представление класса
         """
-        return f"{self.__class__.__name__}.(vacancy_name={self.vacancy_name}, salary={self.salary}, description={self.description}, " \
-               f"url={self.url}, location={self.location}, published={self.published}, experience={self.experience}, " \
-               f"requirements={self.requirements}, education={self.education}, employment={self.employment}"
-
+        return f"{self.__class__.__name__}.(vacancy_name={self.vacancy_name}, salary={self.salary}," \
+               f"description={self.description}, url={self.url}, location={self.location}," \
+               f"published={self.published}, experience={self.experience}, employment={self.employment}"
 
     def __str__(self) -> str:
         """
@@ -50,11 +41,18 @@ class Vacancy:
         """
         return f"Вакансия: {self.vacancy_name}"
 
-
-    def __gt__(self, other: 'Vacancy') -> bool:
+    def __ge__(self, other: 'Vacancy') -> bool:
         """
         Возвращает True, если зарплата вакансии больше зарплаты другой вакансии
         :param other: другая вакансия
         :return: True, если вакансия больше другой
         """
-        return self.salary > other.salary
+        return self.salary >= other.salary
+
+    def __le__(self, other: 'Vacancy') -> bool:
+        """
+        Возвращает True, если дата публикации вакансии меньше даты публикации другой вакансии
+        :param other: другая вакансия
+        :return: True, если вакансия меньше другой
+        """
+        return self.published <= other.published
