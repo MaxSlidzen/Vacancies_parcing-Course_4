46d3c9f4b411d2c1f0e68b1ac90515257d6a9442
Max Slidzen <slidzon94@gmail.com>
2023-06-21 17:11:19 +0300

    added exception handling for connection error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.classes.savers import JSONSaver
 from src.utils.user_interaction import get_query
+import requests
 
 
 def main():
@@ -8,8 +9,12 @@ def main():
     json_saver = JSONSaver("data/parsed_vacancies.json")
     print("В любой момент вы можете завершить работу программы, введя команду 'exit'")
 
-    # Запуск взаимодействия с пользователем
-    get_query(json_saver)
+    # Обработка исключения при проблемах с сервером или интернетом пользователя
+    try:
+        # Запуск взаимодействия с пользователем
+        get_query(json_saver)
+    except requests.exceptions.ConnectionError:
+        print("\nПроизошла ошибка при подключении к серверу. Проверьте подключение к сети или попробуйте позже.")
 
     # Удаление временного файла
     json_saver.clear_data()
